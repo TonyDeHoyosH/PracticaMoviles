52e21bec5ce77bcb682c0d563baa15d79f8ccdf1
TonyDeHoyosH <243776@ids.upchiapas.edu.mx>
2025-11-05 23:59:44 -0600

    Add contents of Compose_navigation2 (remove embedded .git)

---
+++ Compose_navigation2/PracticasMoviles_Compose_navigation2/app/src/main/java/com/example/data/Student.kt
@@ -0,0 +1,7 @@
+package com.example.data
+
+data class Student(
+    val id: Long,
+    val name: String,
+    val description: String,
+)
---
+++ Compose_navigation2/PracticasMoviles_Compose_navigation2/app/src/main/java/com/example/practica_01/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.practica_01
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.practica_01.ui.theme.Practica01Theme
+import com.example.presentation.navigation.NavManager
+import com.example.presentation.views.HomeView
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Practica01Theme(darkTheme = true) {
+                NavManager()
+            }
+        }
+    }
+}
+
